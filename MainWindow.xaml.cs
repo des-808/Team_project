4b903254596fa3d3da4561687b7da094981acd05
Sergey <94394415+des-808@users.noreply.github.com>
2023-11-24 01:16:23 +0300

    xxx

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,54 +20,6 @@ namespace Team_project
             DataContext = ViewModel;
         }
 
-        private void listBoxItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void dataGridItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void addBtn_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void editBtn_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void delBtn_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void listBoxItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void dataGridItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void addBtn_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void editBtn_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void delBtn_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+        
     }
 }
