b9a29f2ba7d2fbf74546ef8f23f0fb479cc3166e
Andrei <ikar2008@hotmail.com>
2023-11-24 23:10:34 +0300

    Revert "Обновление репозитория из локального в глобальный"
    This reverts commit f52cdc7c54d828d0fd3f9618c9f104f9d68bc99a.

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -13,9 +13,6 @@ namespace Team_project.ViewModel
     {
         DbbooksContext db = new DbbooksContext();
         private Book? selectedBook;
-        /// <summary>
-        /// Коллекция для сборки данных для работы с ними.
-        /// </summary>
         public ObservableCollection<Book> BooksObserv { get; set; }
 
         public Book SelectedUser
