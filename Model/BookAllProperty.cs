a7c35cd93578680a13598136985bcde4a15c4c55
Sergey <94394415+des-808@users.noreply.github.com>
2023-11-25 22:01:00 +0300

    добавил общий класс

--- Model/BookAllProperty.cs
+++ Model/BookAllProperty.cs
@@ -8,6 +8,9 @@ namespace Team_project.Model
 {
     class BookAllProperty
     {
-
+        public int BookAllPropertyId { get;private set; }
+        public List<Author>? Authors { get; set; }
+        public List<Book>? Books { get; set; }
+        public List<Category>? Categories { get; set; }
     }
 }
